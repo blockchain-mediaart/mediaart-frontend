63020526155024d1d5c00bebcc835e0d1f8557f6
lucetre <lucetre97@gmail.com>
2020-05-17 21:46:19 +0900

    Install Bootstrap

--- blockchain-mediaart/src/components/dashboard/dashboard.js
+++ blockchain-mediaart/src/components/dashboard/dashboard.js
@@ -1,2 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+class Dashboard extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1>hello</h1>
+                
+                <button type="button" class="btn btn-primary">Primary</button>
+                <button type="button" class="btn btn-secondary">Secondary</button>
+                <button type="button" class="btn btn-success">Success</button>
+                <button type="button" class="btn btn-danger">Danger</button>
+                <button type="button" class="btn btn-warning">Warning</button>
+                <button type="button" class="btn btn-info">Info</button>
+                <button type="button" class="btn btn-light">Light</button>
+                <button type="button" class="btn btn-dark">Dark</button>
+                <button type="button" class="btn btn-link">Link</button>
+            </div>
+        );
+    }
+}
+
+export default Dashboard;
--- blockchain-mediaart/src/index.js
+++ blockchain-mediaart/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Dashboard from './components/dashboard/dashboard';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
