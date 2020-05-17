e8f216bad62b9abef59249788deb441474e09f1d
yunnysunnyshiny <mylosy33@gmail.com>
2020-05-17 21:55:46 +0900

    gallery 1st

--- blockchain-mediaart/src/index.js
+++ blockchain-mediaart/src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Dashboard from './components/dashboard/dashboard';
+import Gallery from './components/gallery/gallery';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Gallery />
   </React.StrictMode>,
   document.getElementById('root')
 );
