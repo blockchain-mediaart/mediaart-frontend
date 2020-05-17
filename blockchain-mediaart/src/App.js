964fbdc2471620d993072f5e4b56f68b1c6ddaac
JSKeum <JSKeum2512@gmail.com>
2020-05-18 00:33:30 +0900

    Add Router - Switch

--- blockchain-mediaart/src/App.js
+++ blockchain-mediaart/src/App.js
@@ -3,6 +3,7 @@ import { Main, Gallery, MyMediaart, Dashboard, Detail } from './components';
 import {
   BrowserRouter as Router,
   Route,
+  Switch
 } from "react-router-dom";
 
 // import './App.css';
@@ -10,23 +11,30 @@ import {
 function App() {
   return (
     <Router>
-    <div className="App">    
-      <Route exact path='/'>
-        <Main />
-      </Route>  
-      <Route exact path='/gallery'>
-        <Gallery />
-      </Route>
-      <Route exact path='/MyMediaart'>
-        <MyMediaart />
-      </Route>
-      <Route exact path='/detail'>
-        <Detail />
-      </Route>
-      <Route exact path='/dashboard'>
-        <Dashboard />
-      </Route>
-    </div>
+    
+      <div className="App">    
+        <Switch>
+          <Route exact path='/'>
+            <Main />
+          </Route>  
+          <Route exact path='/gallery'>
+            <Gallery />
+          </Route>
+          <Route exact path='/MyMediaart'>
+            <MyMediaart />
+          </Route>
+          <Route exact path='/detail'>
+            <Detail />
+          </Route>
+          <Route exact path='/dashboard'>
+            <Dashboard />
+          </Route>
+          <Route path='*'>
+            <h1>PAGE NOT FOUND</h1>
+          </Route>
+        </Switch>
+      </div>
+    
     </Router>
   );
 }
