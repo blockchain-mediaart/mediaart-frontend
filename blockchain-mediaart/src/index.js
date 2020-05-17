eb2ca688710679d9d056131da01cf31cce929ae0
lucetre <lucetre97@gmail.com>
2020-05-17 21:51:21 +0900

    Start dashboard implementation

--- blockchain-mediaart/src/index.js
+++ blockchain-mediaart/src/index.js
@@ -7,7 +7,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Dashboard />
   </React.StrictMode>,
   document.getElementById('root')
 );
