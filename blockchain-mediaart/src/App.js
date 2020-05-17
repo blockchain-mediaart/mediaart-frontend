94b37a563131db58763de1b129ff6e4cccdab5d5
JSKeum <JSKeum2512@gmail.com>
2020-05-18 00:18:38 +0900

    Add Router

--- blockchain-mediaart/src/App.js
+++ blockchain-mediaart/src/App.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import { Main, Gallery, MyMediaart, Dashboard, Detail } from './components';
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom";
 
 // import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Main />
-      <Gallery />
-      <MyMediaart />
-      <Detail />
-      <Dashboard />
+    <Router>
+    <div className="App">    
+      <Route exact path='/'>
+        <Main />
+      </Route>  
+      <Route exact path='/gallery'>
+        <Gallery />
+      </Route>
+      <Route exact path='/MyMediaart'>
+        <MyMediaart />
+      </Route>
+      <Route exact path='/detail'>
+        <Detail />
+      </Route>
+      <Route exact path='/dashboard'>
+        <Dashboard />
+      </Route>
     </div>
+    </Router>
   );
 }
 
