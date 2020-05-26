7662fcd8a01e39421aca357a9ccf48170c3552ae
JSKeum <JSKeum2512@gmail.com>
2020-05-26 23:59:23 +0900

    add metamask provider - web3s/MetamaskProvider.js

--- blockchain-mediaart/src/components/MyMediaart/MyMediaart.js
+++ blockchain-mediaart/src/components/MyMediaart/MyMediaart.js
@@ -2,8 +2,13 @@ import React from 'react';
 import P5Wrapper from 'react-p5-wrapper';
 
 import sketch from '../p5s/sketch'
+import MetamaskProvider from '../web3s/MetamaskProvider';
 
 class MyMediaart extends React.Component {
+  componentDidMount() {
+    MetamaskProvider();
+  }
+
   render() {
     return (
       <div>
