b7b964ab8bfeee0462404750d31adb3166b64c91
JSKeum <JSKeum2512@gmail.com>
2020-05-27 02:26:51 +0900

    fix variables in metamaskProvider

--- blockchain-mediaart/src/components/MyMediaart/MyMediaart.js
+++ blockchain-mediaart/src/components/MyMediaart/MyMediaart.js
@@ -2,12 +2,13 @@ import React from 'react';
 import P5Wrapper from 'react-p5-wrapper';
 
 import sketch from '../p5s/sketch'
-import MetamaskProvider from '../web3s/MetamaskProvider';
+import metamaskProvider from '../web3s/MetamaskProvider';
 
 class MyMediaart extends React.Component {
   componentDidMount() {
-    MetamaskProvider();
+    metamaskProvider();
   }
+
 
   render() {
     return (
--- blockchain-mediaart/src/components/web3s/MetamaskProvider.js
+++ blockchain-mediaart/src/components/web3s/MetamaskProvider.js
@@ -1,29 +1,35 @@
-import { Web3, web3, ethereum } from 'web3';
+import Web3 from 'web3';
 
-export default function MetamaskProvider() {
+export default function metamaskProvider() {
 
   window.addEventListener('load', async () => {
     // Modern dapp browsers...
     if (window.ethereum) {
-      // window.web3 = new Web3(ethereum);
+      // console.log("123 : " +  ethereum);
+      window.web3 = new Web3(window.ethereum);
       try {
         // Request account access if needed
-        await ethereum.enable();
-        // Acccounts now exposed
+        await window.ethereum.enable();
+
+        let account = window.web3.eth.accounts[0];
+        alert("account :" + account);
+        // Acccounts no w exposed
         // web3.eth.sendTransaction({/* ... */});
       } catch (error) {
         // User denied account access...
+        // console.log("error : " + error);
       }
     }
     // Legacy dapp browsers...
     else if (window.web3) {
-      window.web3 = new Web3(web3.currentProvider);
+      window.web3 = new Web3(window.web3.currentProvider);
       // Acccounts always exposed
       // web3.eth.sendTransaction({/* ... */});
     }
     // Non-dapp browsers...
     else {
       console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+      alert("크롬을 사용해주세요")
     }
   });
 }
