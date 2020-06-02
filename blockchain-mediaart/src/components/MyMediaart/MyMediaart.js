540581fb15477b9428c2e96dcb655e868057c53f
JSKeum <54926767+JSKeum@users.noreply.github.com>
2020-06-03 03:55:48 +0900

    Update MyMediaart.js
    remove unused code

--- blockchain-mediaart/src/components/MyMediaart/MyMediaart.js
+++ blockchain-mediaart/src/components/MyMediaart/MyMediaart.js
@@ -34,14 +34,10 @@ class MyMediaart extends React.Component {
 
   handleChange(event) {
     this.setState({value: event.target.value, code: null});
-
-    console.log("code : " + this.state.value);
   }
 
   changeCode() {
     this.setState({code: this.state.value});
-    console.log("code : " + this.state.code);
-
   }
 
   modifyP5WhenError(val){
@@ -52,16 +48,11 @@ class MyMediaart extends React.Component {
   inputCode() {
     return (
       <div>
-      {/* <form onSubmit={this.handleSubmit}> */}
-        {/* <form> */}
           <label>
             Name:
             <textarea rows="10" cols="70" value={this.state.value} onChange={this.handleChange}></textarea>
-            {/* <input type="textarea" size="100" value={this.state.value} onChange={this.handleChange} /> */}
           </label>
           <button onClick={this.changeCode} > 코드 반영하기</button>
-          {/* <input type="button" value="Submit" onclick={this.changeCode} /> */}
-      {/* </form> */}
       </div>
     );
   }
