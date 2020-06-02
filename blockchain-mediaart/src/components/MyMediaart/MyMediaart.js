041b71c5c2488b0f5195b481f1a54a81daeadc14
JSKeum <JSKeum2512@gmail.com>
2020-06-03 03:52:08 +0900

    reflect users p5 code input to mediaart canvas

--- blockchain-mediaart/src/components/MyMediaart/MyMediaart.js
+++ blockchain-mediaart/src/components/MyMediaart/MyMediaart.js
@@ -8,24 +8,64 @@ class MyMediaart extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {value: '', code: null}
+    this.handleChange = this.handleChange.bind(this);
+    this.inputCode = this.inputCode.bind(this);
+    this.changeCode = this.changeCode.bind(this);
+    this.modifyP5WhenError = this.modifyP5WhenError.bind(this);
   }
 
   componentDidMount() {
     checkEtherConnected()
   }
 
-  // connectToEthereum() {
-
-  // }
-
   render() {
+
+    const inputCode = this.inputCode();
     return (
       <div>
         <Web3ProviderConnectButton />
-        <P5withWeb3 />
+        <P5withWeb3 codeInput={this.state.code} />
+        {inputCode}
+      </div>
+    );
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value, code: null});
+
+    console.log("code : " + this.state.value);
+  }
+
+  changeCode() {
+    this.setState({code: this.state.value});
+    console.log("code : " + this.state.code);
+
+  }
+
+  modifyP5WhenError(val){
+    this.setState({code: val})
+    // console.log(val);
+  }
+
+  inputCode() {
+    return (
+      <div>
+      {/* <form onSubmit={this.handleSubmit}> */}
+        {/* <form> */}
+          <label>
+            Name:
+            <textarea rows="10" cols="70" value={this.state.value} onChange={this.handleChange}></textarea>
+            {/* <input type="textarea" size="100" value={this.state.value} onChange={this.handleChange} /> */}
+          </label>
+          <button onClick={this.changeCode} > 코드 반영하기</button>
+          {/* <input type="button" value="Submit" onclick={this.changeCode} /> */}
+      {/* </form> */}
       </div>
     );
   }
 }
 
 export default MyMediaart;
+
--- blockchain-mediaart/src/components/p5s/P5WithWeb3.js
+++ blockchain-mediaart/src/components/p5s/P5WithWeb3.js
@@ -5,16 +5,21 @@ import P5Wrapper from 'react-p5-wrapper';
 import sketch from "./sketch";
 
 
-export default function P5WithWeb3() {
+export default function P5WithWeb3(props) {
   const web3Context = useWeb3();
   const { networkId, networkName, accounts } = web3Context;
+
+  const code = "p.normalMaterial();p.fill(100, 200, 230);p.translate(0, 0, 0);p.push();p.rotateZ(p.frameCount * 0.01);p.rotateX(p.frameCount * 0.01);p.rotateY(p.frameCount * 0.01);p.cylinder(80, 80);p.pop();";
+
+  let codeInput = props.codeInput;
+
   let color = 100;
 
   return (
     <div className="provider">
       <h1>나의 미디어 아트</h1>
       {accounts && accounts.length && networkId === 3 ?
-        <P5Wrapper sketch={sketch} color={100} />
+        <P5Wrapper sketch={sketch} code = {codeInput ? codeInput : code}/>
         : accounts && accounts.length ?
           <h4>Ropsten Testnet이 아닌 {networkName} 이더리움에 연결되어 있어 나의 미디어 아트를 불러올 수 없습니다. Ropsten Testnet으로 네트워크 설정을 변경해 주세요</h4>
           : <h4>이더리움 네트워크에 연결되지 않아 나의 미디어 아트를 불러올 수 없습니다.</h4>
@@ -23,5 +28,3 @@ export default function P5WithWeb3() {
     </div>
   );
 }
-
-
